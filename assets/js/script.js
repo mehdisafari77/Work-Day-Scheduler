6effb65e397736d307065db0b91f122b250403eb
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-09-29 15:48:39 -0700

    add updateCss function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $("#currentDay").text(moment().format("dddd, MMMM, Do"))
-var currentHour = moment().hours()
+var currentTime = moment().hours()
 
 /*
 1. Use save button to grab value of text area
@@ -15,4 +15,32 @@ $(".saveBtn").on("click", function(){
     var divKey = $(this).parent().attr("id")
     localStorage.setItem(divKey, textValue)
 })
+
+$("#9 .textValue").val(localStorage.getItem("9"))
+$("#10 .textValue").val(localStorage.getItem("10"))
+$("#11 .textValue").val(localStorage.getItem("11"))
+$("#12 .textValue").val(localStorage.getItem("12"))
+$("#13 .textValue").val(localStorage.getItem("13"))
+$("#14 .textValue").val(localStorage.getItem("14"))
+$("#15 .textValue").val(localStorage.getItem("15"))
+$("#16 .textValue").val(localStorage.getItem("16"))
+$("#17 .textValue").val(localStorage.getItem("17"))
+
+function updateCss() {
+    $("timeDiv").each(function() {
+        var divId = $(this).attr("id")
+        
+        if (currentTime === divId) {
+            $(this).addClass("present")
+        } else if (currentTime <= divId) {
+            $(this).add("past")
+        } else {
+            $(this).addClass("future")
+        }
+
+    })
+}
+
+
+
 
